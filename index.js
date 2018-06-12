9d40160a63928bf3529bce39ade49d81ce2a7da8
sophia-cheng <39381903+sophia-cheng@users.noreply.github.com>
2018-06-11 23:48:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 insert code
+The lambda function itself in the form of index.js also must be submitted. All documentation for found code must be shown by commenting
+above the used code on where it came from. This should always include the url for where you found it. If documentation is not given and it is found
+that you used code from another source, this can be grounds for plagiarism and will result in a zero on the final.
